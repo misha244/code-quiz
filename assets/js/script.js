c426b78d8541da0b0cccf03ae65ec0260760be75
Misha <73886737+misha244@users.noreply.github.com>
2021-03-22 18:22:04 +0000

    add event listener and timer value

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
-const startQuiz = document.getElementById("start-btn");
+const startButton = document.getElementById("start-btn");
+const startQuiz = document.getElementById("start-quiz");
 
 const timerSpan = document.getElementById("timer");
+
+let timerValue = 60;
+
+startButton.addEventListener("click", startQuiz);
