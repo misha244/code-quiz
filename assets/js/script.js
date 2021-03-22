50833d68c90f6f791cfa75d4dab8d652a910e2b5
Misha <73886737+misha244@users.noreply.github.com>
2021-03-22 14:35:16 +0000

    prep edits

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,17 @@
 //const startQuiz
 // removes info section
-// and starts timer - use setInterval function (fn, 1000)
+// and starts timer - use setInterval(fn, 1000) *call fn something like timerTick(?)
+//// timeValue -1
+//// target timer span
+//// set content
+///// if timer === 0 -> end of quiz form - all done, enter initial, view highscore, etc
 // declare timeValue = 60
+
+//form div - create in JS and append to body -> fn createAndAppendForm ()
+
+// submitScore -> fn
+//// get score
+//// get initials/name from input
+//// construct string -> e.g. - "AB - 22"
+//// store string in local storage
+//// -> navigate to high-scores.html
