40ce92ca29cefffac8972ac56f159771941a447f
Misha <73886737+misha244@users.noreply.github.com>
2021-03-22 18:47:11 +0000

    adding placeholder questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,4 +5,33 @@ const timerSpan = document.getElementById("timer");
 
 let timerValue = 60;
 
+// questions array
+questions = [
+  {
+    title: "Who let the dogs out?",
+    choices: [
+      "The Baha Men",
+      "The Illuminati",
+      "Elon Musk",
+      "The dogs let themselves out",
+    ],
+    answer: "Elon Musk",
+  },
+  {
+    title: "Moon landing?",
+    choices: [
+      "staged by Walt Disney",
+      "sure",
+      "Alexa, show Buzz Aldrin punching that dude",
+      "alexa, play man on the moon by r.e.m.",
+    ],
+    answer: "sure",
+  },
+  {
+    title: "Do you know who I am!?!",
+    choices: ["Who?!", "Ronnie Pickering!", "Who the fuck is that?", "Me"],
+    answer: "Ronnie Pickering",
+  },
+];
+
 startButton.addEventListener("click", startQuiz);
