274347c009f80b1e2fcf409e941c4275ea12d4e1
Misha <73886737+misha244@users.noreply.github.com>
2021-03-27 11:14:11 +0000

    add todo list

---
+++ assets/js/highscores.js
@@ -0,0 +1,11 @@
+const bodyElement = document.body;
+const goBackButton = document.getElementById("go-back-btn");
+const clearScoreButton = document.getElementById("clear-btn");
+
+// TODO
+// on load - read local storage & remove from page
+/// create li for each score item
+/// append to score div
+// create high scores array
+// clear score fn - remove from local storage
+// go back fn - navigate to index.html
