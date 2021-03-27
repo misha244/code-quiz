dd30dafbb887ca0dd5c86adbb77c943cce0f4c45
Misha <73886737+misha244@users.noreply.github.com>
2021-03-27 16:18:38 +0000

    create goBack fn

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -2,10 +2,15 @@ const bodyElement = document.body;
 const goBackButton = document.getElementById("go-back-btn");
 const clearScoreButton = document.getElementById("clear-btn");
 
+// go back fn - navigate to index.html
+const goBack = () => {
+  location.href = "/index.html";
+};
+
 // TODO
+// fn to get scores from storage
 // on load - read local storage & remove from page
 /// create li for each score item
 /// append to score div
 // create high scores array
 // clear score fn - remove from local storage
-// go back fn - navigate to index.html
