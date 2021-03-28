6c33c0a541e9e5db69a1a98293b0db8cc972b04d
Misha <73886737+misha244@users.noreply.github.com>
2021-03-28 13:37:50 +0100

    delete rank fn for now

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -4,18 +4,20 @@ const clearScoreButton = document.getElementById("clear-btn");
 
 let index = 0;
 
-// rank scores in descending order
-const rankScores = (lowestScore, highestScore) =>
-  highestScore.score - lowestScore.score;
-
-// clear score fn - remove from local storage
-function clearScore() {
-  localStorage.clear();
-}
-
 // declare high scores const
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 console.log(highScores);
+
+// rank scores in descending order
+const rankScores = (lowestScore, highestScore) => {
+  highestScore.score - lowestScore.score;
+}; //todo define lower
+
+const score =
+  // clear score fn - remove from local storage
+  function clearScore() {
+    localStorage.clear();
+  };
 
 // on load - read local storage & remove from page
 const onLoad = () => {
@@ -29,8 +31,11 @@ window.addEventListener("load", onLoad);
 goBackButton.addEventListener("click", goBack);
 clearScoreButton.addEventListener("click", clearScore);
 
+// create high scores table/list using the scores in local storage
+const createHighScoresTable = () => {};
+
 // TODO
-// create high scores table using local storage
+
 /// create li for each score item
 /// append to score div
 // render high scores table
